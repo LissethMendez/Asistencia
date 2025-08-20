a15cbca0908a4110e364da8390edd591a9085516
Fátima Lisseth Jiménez Mendez <fatimalissjimenez@outlook.com>
2025-08-19 23:50:13 -0600

    funcionalidad completa

--- Asistencia/Asistencia-Esfe/src/main/java/com/Esfe/Asistencia_Esfe/Repositorios/IGrupoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.esfe.Asistencia.Repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.esfe.Asistencia.Modelos.Grupo;
-
-public interface IGrupoRepository extends JpaRepository<Grupo,Integer> {
-
-}
